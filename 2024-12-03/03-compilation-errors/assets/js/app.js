5f281f5ef718b841275a417ef729bf9f6d1827cc
noop <beeryba05@gmail.com>
2024-12-03 11:00:22 +0200

    remove old app.js and index.html files; add new files for compilation and runtime error examples

---
+++ 2024-12-03/03-compilation-errors/assets/js/app.js
@@ -0,0 +1,6 @@
+console.log(`hello`)
+// const firstName = 'shahar;
+console.log(`hello again`)
+console.log(firstName)
+
+//compilation error will result in a completely not runing code
---
+++ 2024-12-03/04-runtime-error/assets/js/app.js
@@ -0,0 +1,8 @@
+console.log(`hello1`)
+const numberOfDigits = +prompt(`enter number of digits`)
+
+const pi = Math.PI
+
+console.log(pi.toFixed(numberOfDigits))
+
+console.log(`hello2`)
