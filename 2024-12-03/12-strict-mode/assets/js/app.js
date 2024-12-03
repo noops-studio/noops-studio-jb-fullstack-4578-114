f46bc9982bf0fec89c00565e0b7b3e4bede2fb5a
noop <beeryba05@gmail.com>
2024-12-03 14:43:07 +0200

    add initial HTML, CSS, and JavaScript files for strict mode example

---
+++ 2024-12-03/12-strict-mode/assets/js/app.js
@@ -0,0 +1,28 @@
+// const message = 'hello world'
+// console.log(message)
+// console.log(message[1])
+// message[1]= 'o'
+// console.log(message)
+
+// let counter = 3
+// console.log(counter)
+// count=4
+// console.log(counter)
+// alert(count)
+
+
+// alert = message => console.log
+
+"use strict";
+const message = 'hello world'
+console.log(message)
+console.log(message[1])
+console.log(message)
+
+let counter = 3
+console.log(counter)
+console.log(counter)
+alert(count)
+
+
+alert = message => console.log
