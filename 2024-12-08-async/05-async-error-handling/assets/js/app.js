eb51ca91b7ae0475bd92439b573734452b9685cc
beery <beeryba05@gmail.com>
2024-12-08 11:30:09 +0200

    refactor async color picker; enhance error handling with callbacks and improve code structure

--- 2024-12-08-async/05-async-error-handling/assets/js/app.js
+++ 2024-12-08-async/05-async-error-handling/assets/js/app.js
@@ -5,7 +5,7 @@
     const pickColor = () => {
         const colors = ['red', 'green', 'blue', 'yellow', 'cyan']
         const randomColor = colors[Math.floor(Math.random() * colors.length * 2)]
-        if (!randomColor) throw new Error('undefined color')
+        if(!randomColor) throw new Error ('undefined color')
         document.body.style.backgroundColor = randomColor
     }
 
@@ -20,26 +20,26 @@
 
     ////////////////////////////////////////////////////////////////////////////////////
 
-    const pickColorAsync = (callback) => {
+    const pickColorAsync = (successCallback, errorCallback) => {
         setTimeout(() => {
             const colors = ['red', 'green', 'blue', 'yellow', 'cyan']
             const randomColor = colors[Math.floor(Math.random() * colors.length * 2)]
-            if (!randomColor) throw new Error('undefined color')
-            callback(randomColor)
+            if(!randomColor) {
+                errorCallback('color is undefined')
+                return
+            }
+            successCallback(randomColor)
         }, 2000)
     }
 
     document.getElementById('switchColorAsync').addEventListener('click', () => {
-        try {
-            pickColorAsync(color => {
-                document.body.style.backgroundColor = randomColor
-            }, error => {
-                console.log(error)
-            }
-            )
-        } catch (e) {
-            console.log(e.message)
+        const colorPicked = color => {
+            document.body.style.backgroundColor = color
         }
+        const failure = error => {
+            console.log(error)
+        }
+        pickColorAsync(colorPicked, failure)
     })
 
 
