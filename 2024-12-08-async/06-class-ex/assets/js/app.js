b95486bfa50a06e7378cdabff2dc0a3f3a033438
beery <beeryba05@gmail.com>
2024-12-08 11:49:07 +0200

    add initial HTML, CSS, and JavaScript files for async power calculation example

---
+++ 2024-12-08-async/06-class-ex/assets/js/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    function getPower(num, successCallback, errorCallback) {
+        setTimeout(() => {
+            if (num % 3 === 0) return errorCallback('number cant be divisible by 3');
+            successCallback(num ** 2);
+        }, 3000); 
+    }
+
+    document.getElementById('numberator').addEventListener('click', () => {
+        const number = +prompt('Enter a number');
+        getPower(number, (result) => {
+            console.log(result);
+        }, (error) => {
+            console.error(error);
+        });
+    });
+});
