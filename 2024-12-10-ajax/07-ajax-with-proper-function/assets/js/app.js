e19abdbae0f5e75a6bcbd601ae35c31d270aa676
beery <beeryba05@gmail.com>
2024-12-10 11:38:54 +0200

    refactor user data retrieval logic; extract async functions for fetching and generating HTML from user data

--- 2024-12-10-ajax/07-ajax-with-proper-function/assets/js/app.js
+++ 2024-12-10-ajax/07-ajax-with-proper-function/assets/js/app.js
@@ -1,18 +1,27 @@
 "use strict";
 
 (() => {
-    document.getElementById('button').addEventListener('click', async () => {
-        const url = 'https://jsonplaceholder.typicode.com/users';
-        
-        const response = await fetch(url);
-        const users = await response.json();
-        console.log(users);
 
-        const newHtml = users.map(user => {
-            const { name, email } = user;
-            return `<li>name: ${name}, email: ${email}</li>`;
-        }).reduce((acc, next) => `${acc} ${next}`, '');
+const getData = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
+}
 
-        document.getElementById('users').innerHTML = newHtml;
-    });
+const generateHtml = async (url) => {
+    const users = await getData(url);
+    const newHtml = users.map(user => {
+        const { name, email } = user;
+        return `<li>name: ${name}, email: ${email}</li>`;
+    }).reduce((acc, next) => `${acc} ${next}`, '');
+    return newHtml;
+}
+
+
+
+document.getElementById('button').addEventListener('click', async () => {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const newHtml = await generateHtml(url);
+    document.getElementById('users').innerHTML = newHtml;
+});
+
 })();
