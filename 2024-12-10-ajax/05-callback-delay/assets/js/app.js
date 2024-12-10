dba9fcc39e3de9208c9068af2d37110b2f622ae4
beery <beeryba05@gmail.com>
2024-12-10 10:44:04 +0200

    add callback delay functionality; implement setTimeout to log message after button click

--- 2024-12-10-ajax/05-callback-delay/assets/js/app.js
+++ 2024-12-10-ajax/05-callback-delay/assets/js/app.js
@@ -5,8 +5,12 @@
     document.getElementById('button').addEventListener('click', () => {
 
         document.body.style.backgroundColor = "Green";
-        alert("Done");
-        
+        setTimeout(()=>{
+            console.log('1 seccond passed')
+        },1000)
+for (let s = 0; s < array.length; s++) {
+
+}
     });
 
 })();
