92d2259e5f2c0bbfa93d1a9c82c01611955b65c0
beery <beeryba05@gmail.com>
2024-12-15 16:02:23 +0200

    feat: add jQuery functionality for button interactions and style updates

--- 2024-12-15-jquary-complecsity/09-jquary/assets/js/app.js
+++ 2024-12-15-jquary-complecsity/09-jquary/assets/js/app.js
@@ -1,6 +1,16 @@
-"use strict";
-
-(() => {
-    
-  
-})();
+$(() => {
+  $("#button").css("color", "red");
+  $("#button").click(function (event) {
+    $(this).css("color", "blue");
+    $(this).fadeOut(2000).fadeIn(2000);
+    $(this).addClass("tamir");
+    $("div").each(function () {
+      $(this).css("background-color", "green");
+    });
+  });
+  $.getJSON("https://jsonplaceholder.typicode.com/todos", data,
+    function (data) {
+        console.log(data);
+    }
+  );
+});
