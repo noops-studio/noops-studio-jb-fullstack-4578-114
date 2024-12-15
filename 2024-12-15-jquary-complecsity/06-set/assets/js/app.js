47622f65d3736f6d8c6d081755646295355a1328
beery <beeryba05@gmail.com>
2024-12-15 13:56:45 +0200

    feat: add functionality to create a set from an array and log unique values

--- 2024-12-15-jquary-complecsity/06-set/assets/js/app.js
+++ 2024-12-15-jquary-complecsity/06-set/assets/js/app.js
@@ -3,6 +3,13 @@
 (() => {
     
 const mySet = new Set();
-console.log(mySet);
+mySet.add('Beery');
+mySet.add('Beery');
+mySet.add('Yuval');
+mySet.add('Mishel');
 
+const myNonUniqueArray = [10,10,20,20,30,30,40,40,50,50];
+const myUniqueSet = new Set([10,10,20,20,30,30,40,40,50,50]);
+const myUniqueArray = [...myUniqueSet];
+console.log(myUniqueArray)
 })();
