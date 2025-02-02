8e49e1e1d1f35108a55dce8e35488fff79e551d5
beery <beeryba05@gmail.com>
2025-02-02 14:09:37 +0200

    feat: add /list endpoint to return user details; implement user list functionality in server.js

--- 2025-02-02-node/11-class-ex/server.js
+++ 2025-02-02-node/11-class-ex/server.js
@@ -8,16 +8,49 @@ const user = {
   age: 23,
 };
 
+const users = [
+  {
+    name: "Yuval",
+    email: "yuval@gmail.com",
+    age: 23,
+},
+{
+    name: "Yuval",
+    email: "yuval@gmail.com",
+    age: 23,
+},  {
+    name: "Yuval",
+    email: "yuval@gmail.com",
+    age: 23,
+}];
+
+
 // /user endpoint to echo the user object
 
-const requestListener = (request, Response) => {
-  if (request.url === "/user") {
-    console.log("Request to /user endpoint");
-    Response.write(JSON.stringify(user));
-  } else if (request.url === "/") {
-    console.log("Request to / endpoint");
+const requestListener = (request, response) => {
+  switch (request.url) {
+    case "/user":
+      console.log("Request to /user endpoint");
+      response.setHeader("Content-Type", "application/json");
+      response.write(JSON.stringify(user));
+      break;
+    case "/list":
+      response.setHeader("Content-Type", "text/plain");
+      response.write("name,email,age\n");
+      users.forEach((user) => {
+        response.write(`${user.name},${user.email},${user.age}\n`);
+      });
+      break;
+    case "/":
+      response.setHeader("Content-Type", "application/json");
+      console.log("Request to / endpoint");
+      break;
+    default:
+      response.setHeader("Content-Type", "text/plain");
+      response.write("404 Not Found");
+      break;
   }
-  Response.end();
+  response.end();
 };
 
 createServer(requestListener).listen(3000);
