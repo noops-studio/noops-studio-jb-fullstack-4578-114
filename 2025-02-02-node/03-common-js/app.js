ce7198729f466bfd09d3d6281304725f81adb4e5
beery <beeryba05@gmail.com>
2025-02-02 10:14:24 +0200

    feat: add basic Node.js application; implement sum function in calc module; create app to calculate sum from command line arguments

---
+++ 2025-02-02-node/03-common-js/app.js
@@ -0,0 +1,5 @@
+const {sum} = require('./calc');
+const number1 = +process.argv[2];
+const number2 = +process.argv[3];
+
+console.log(`result: ${sum(number1, number2)}`);
---
+++ 2025-02-02-node/03-common-js/calc.js
@@ -0,0 +1,8 @@
+const sum = (a, b) => {
+    return a + b
+};
+
+
+module.exports = {
+    sum
+}
