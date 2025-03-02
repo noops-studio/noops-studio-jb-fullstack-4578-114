0ad900b7c29c44724e019c75399b4568c520628a
beery <beeryba05@gmail.com>
2025-03-02 14:23:36 +0200

    feat: add file upload middleware and update configurations; disable logging and include express-fileupload dependency

--- 2025-03-02/social-network/backend/src/app.ts
+++ 2025-03-02/social-network/backend/src/app.ts
@@ -6,6 +6,7 @@ import errorResponder from "./middlewares/error/error-responder";
 import notFound from "./middlewares/not-found";
 import router from "./routers/index";
 import cors from 'cors';
+import fileUpload from "express-fileupload";
 const port = config.get<string>('app.port');
 const name = config.get<string>('app.name');
 const force = config.get<boolean>('sequelize.sync.force');
@@ -21,10 +22,16 @@ app.use(cors())
 // }));
 
 app.use(express.json());
+app.use(fileUpload());
 app.use(express.urlencoded({ extended: true }));
+
 (async () => {
     await sequelize.sync({ force });
 
+    app.use((req, res, next) => {
+        console.log(`Method: ${req.method}  Request URL: ${req.originalUrl}`);
+        next(); // Move to the next middleware
+    });
     // Use the main router
     app.use('/api', router);
 
